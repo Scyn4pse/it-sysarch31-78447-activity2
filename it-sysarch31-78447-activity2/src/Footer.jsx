060819b31add005e38837d6d1cb3c977086e7dde
Scyn4pse <canquechiara76@gmail.com>
2024-03-02 09:12:01 +0800

    Completed Tutorial

---
+++ it-sysarch31-78447-activity2/src/Footer.jsx
@@ -0,0 +1,13 @@
+
+function Footer(){
+
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} IT-SYSARCH31-78447 Made by Chiara Marie Canque</p>
+        </footer>
+
+    );
+
+}
+
+export default Footer
