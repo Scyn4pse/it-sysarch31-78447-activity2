cc959e1945537620ce50d1fd566635af24fd0b28
Scyn4pse <canquechiara76@gmail.com>
2024-03-02 10:33:36 +0800

    Added a Content file for the array

--- it-sysarch31-78447-activity2/src/Student.jsx
+++ it-sysarch31-78447-activity2/src/Student.jsx
@@ -1,9 +1,7 @@
+import students from './Content.jsx'
 
-
-function Student(props){
-    const studList = props.stud;
-    const category = props.category;
-    const listStuds = studList.map(stud => <div className="card">
+function Student(){
+    const listStuds = students.map(stud => <div className="card">
                                             <img className="card-image" src="https://via.placeholder.com/150" alt="profile picture"></img>
                                             <h2 className="card-title">{stud.name}</h2>
                                             <p className="card-text">{stud.email}</p>
